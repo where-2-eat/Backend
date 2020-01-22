434a4c57a12c1d68c7058c02adbe3591d8ab0133
Irmak Parkis <nihal.pakis@mail.mcgill.ca>
2020-01-22 18:47:47 -0500

    Initial commit of the backend application :tada:

---
+++ Where2eat-Backend/src/main/java/ca/mcgill/ecse428/where2eat/backend/Where2eatBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.where2eat.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Where2eatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Where2eatBackendApplication.class, args);
+	}
+
+}
---
+++ Where2eat-Backend/src/test/java/ca/mcgill/ecse428/where2eat/backend/Where2eatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.where2eat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Where2eatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
