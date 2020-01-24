aed4a15f50623ca3294d2fb19a24d5a85b6ac731
Imad Dodin <imad.dodin@mail.mcgill.ca>
2020-01-23 22:44:08 -0500

    Initialize Database and Configure Gradle :art: (#1)
     Resolves #1
    
     - Adds index route for testing
     - Configures Gradle settings
     - Configures Heroku Procfile
     - Configures PostgreSQL Database Driver

--- src/main/java/ca/mcgill/ecse428/where2eat/backend/Where2eatBackendApplication.java
+++ src/main/java/ca/mcgill/ecse428/where2eat/backend/Where2eatBackendApplication.java
@@ -2,12 +2,20 @@ package ca.mcgill.ecse428.where2eat.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Where2eatBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Where2eatBackendApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello from Where2Eat!";
 	}
 
 }
