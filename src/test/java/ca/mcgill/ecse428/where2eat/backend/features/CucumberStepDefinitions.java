ebaef8eb804cf38db2e0b8284e7fbbd486207006
thomasp05 <thom_phil@videotron.ca>
2020-02-02 13:40:41 -0500

    Set up cucumber testing framework in gradle project.

---
+++ src/test/java/ca/mcgill/ecse428/where2eat/backend/features/CucumberStepDefinitions.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse428.where2eat.backend.features;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.After;
+import io.cucumber.java.en.*;
+import java.io.*;
+import java.sql.Time;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CucumberStepDefinitions {
+	
+}
